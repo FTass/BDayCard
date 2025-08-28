a148e2e75237e6bbaf432cfdcd1c58190891e0e1
FTass <f.bustamantegajardo@uandresbello.edu>
2025-08-27 20:33:25 -0400

    feat/styling + images

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-const dialogos = [
-  "🎂 ¡Hola, mi amor! Hoy es un día muy especial...",
-  "✨ Porque hoy celebramos que el mundo te tiene a ti.",
-  "🐶 Estaba pensando... ¿y si hacemos algo distinto?",
-  "🎁 Tengo algo preparado para ti, pero primero...",
-  "💬 Quiero contarte una pequeña historia...",
-  "👉 Presiona siguiente y comencemos esta mini aventura.",
-];
-const fondos = [
-    "images/fondo-1.jpg",
-    "images/fondo-2.jpg",
-    "images/fondo-3.jpg",
-    "images/fondo-4.jpg",
-    "images/fondo-5.jpg",
-]
+import { escenas } from "./escenas.js";
+
 let indice = 0;
+
 const textoElemento = document.getElementById("dialog-text");
-const botonSiguiente = document.getElementById("next-btn");
-const imgElemento = document.getElementById("f1")
-function mostrarDialogo() {
-  if (indice < dialogos.length) {
-    textoElemento.textContent = dialogos[indice];
-    imgElemento.src= fondos[indice]
+const botonSiguiente = document.getElementById("btn-text");
+const imgElemento = document.getElementById("f1");
+
+function mostrarEscena() {
+  if (indice < escenas.length) {
+    const escena = escenas[indice];
+    textoElemento.textContent = escena.texto;
+    imgElemento.src = escena.imagen;
+    if (escena.color) {
+      document.body.classList.remove("bg-light");
+      document.body.style.backgroundColor = escena.color;
+    }
     indice++;
   } else {
-    textoElemento.textContent = "🎉 ¡Fin de la introducción! (Aquí puede aparecer tu tarjeta o continuar la historia)";
+    textoElemento.textContent =
+      "🎉 ¡Fin de la introducción! (Aquí puede aparecer tu tarjeta o continuar la historia)";
     botonSiguiente.disabled = true;
     botonSiguiente.textContent = "✨";
   }
 }
+
 const musica = document.getElementById("bg-music");
 
-document.addEventListener("click", () => {
-  if (musica.paused) {
-    musica.play();
-  }
-}, { once: true }); // se ejecuta solo una vez
+document.addEventListener(
+  "click",
+  () => {
+    if (musica.paused) {
+      musica.play();
+    }
+  },
+  { once: true }
+); // se ejecuta solo una vez
 
-botonSiguiente.addEventListener("click", mostrarDialogo);
-
+botonSiguiente.addEventListener("click", mostrarEscena);
 // Mostrar el primer diálogo al cargar
-mostrarDialogo();
+mostrarEscena();
